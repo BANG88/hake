b97ead2a5bb314a174658b553b0d2b61a7528c0c
bang <sqibang@gmail.com>
2018-03-20 10:45:46 +0800

    chore: upgrade react to latest

--- typings/dev.d.ts
+++ typings/dev.d.ts
@@ -1,47 +1,35 @@
 // A hack for the Redux DevTools Chrome extension.
 
-
-
 /**
  * require
- * 
+ *
  * @interface WebpackRequireEnsureCallback
  */
 interface WebpackRequireEnsureCallback {
-    (req: WebpackRequire): void
+  (req: WebpackRequire): void;
 }
 
 /**
  * If you has installed @types/node please uninstall it, we use require.ensure for code-splitting
  * and these definitions if for webpack require function use...
  */
-interface WebpackRequire {
-    (id: string): any;
-    (paths: string[],
-        callback: (...modules: any[]) => void
-    ): void;
-    ensure(ids: string[],
-        callback: WebpackRequireEnsureCallback,
-        chunkName?: string
-    ): void;
-    context(directory: string,
-        useSubDirectories?: boolean,
-        regExp?: RegExp
-    ): WebpackContext;
+interface WebpackRequire extends NodeRequire{
+  (id: string): any;
+  (paths: string[], callback: (...modules: any[]) => void): void;
+  ensure(
+    ids: string[],
+    callback: WebpackRequireEnsureCallback,
+    chunkName?: string
+  ): void;
+  context(
+    directory: string,
+    useSubDirectories?: boolean,
+    regExp?: RegExp
+  ): WebpackContext;
 }
 
 interface WebpackContext extends WebpackRequire {
-    keys(): string[];
+  keys(): string[];
 }
 
 declare var require: WebpackRequire;
-
-/**
- * process
- */
-declare var process: {
-    env: {
-        NODE_ENV?: string
-        [key: string]: any
-    }
-}
